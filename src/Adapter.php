20d542c140e91a2435469fd3d986a96790f4a78c
Jan Henckens <jan@henckens.be>
2023-10-26 16:47:49 +0200

    Working plugin

---
+++ src/Adapter.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace studioespresso\mailersend;
+
+use Craft;
+use craft\behaviors\EnvAttributeParserBehavior;
+use craft\helpers\App;
+use craft\mail\transportadapters\BaseTransportAdapter;
+use Symfony\Component\Mailer\Bridge\MailerSend\Transport\MailerSendApiTransport;
+
+/**
+ * Adapter represents the MailerSend mail adapter.
+ *
+ * @author Studio Espresso <support@studioespresso.co>
+ * @since 1.0
+ */
+class Adapter extends BaseTransportAdapter
+{
+    /**
+     * @inheritdoc
+     */
+    public static function displayName(): string
+    {
+        return 'MailerSend';
+    }
+
+    /**
+     * @var string
+     */
+    public ?string $token = null;
+
+    /**
+     * @var string|null
+     */
+    public ?string $messageStream = null;
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'token' => Craft::t('postmark', 'Token'),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors(): array
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['parser'] = [
+            'class' => EnvAttributeParserBehavior::class,
+            'attributes' => [
+                'token',
+            ],
+        ];
+        return $behaviors;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defineRules(): array
+    {
+        return [
+            [['token'], 'required'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSettingsHtml(): ?string
+    {
+        return Craft::$app->getView()->renderTemplate('mailersend/settings', [
+            'adapter' => $this,
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function defineTransport(): array|\Symfony\Component\Mailer\Transport\AbstractTransport
+    {
+        $transport = new MailerSendApiTransport(App::parseEnv($this->token));
+        return $transport;
+    }
+}
---
+++ src/Plugin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace studioespresso\mailersend;
+
+use craft\events\RegisterComponentTypesEvent;
+use craft\helpers\MailerHelper;
+use yii\base\Event;
+
+/**
+ * Plugin represents the MailerSend plugin.
+ *
+ * @author Studio Espresso <support@studioespresso.co>
+ * @since 1.0
+ */
+class Plugin extends \craft\base\Plugin
+{
+    /**
+     * @inheritdoc
+     */
+    public string $schemaVersion = '1.0.0';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        Event::on(
+            MailerHelper::class,
+            MailerHelper::EVENT_REGISTER_MAILER_TRANSPORT_TYPES,
+            function(RegisterComponentTypesEvent $event) {
+                $event->types[] = Adapter::class;
+            }
+        );
+    }
+}
